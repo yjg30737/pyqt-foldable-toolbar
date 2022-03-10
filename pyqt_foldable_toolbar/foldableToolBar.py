302d0e788f80b6b59cc8616c6e81c6da58e3a13b
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-10 19:08:19 +0900

    Add animation

--- pyqt_foldable_toolbar/foldableToolBar.py
+++ pyqt_foldable_toolbar/foldableToolBar.py
@@ -1,11 +1,14 @@
 from PyQt5.QtWidgets import QToolBar, QWidget, QHBoxLayout, QSizePolicy, QAction, QWidgetAction
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPropertyAnimation, QAbstractAnimation
 from pyqt_svg_icon_pushbutton import SvgIconPushButton
 
 
 class FoldableToolBar(QToolBar):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.__initUi()
+
+    def __initUi(self):
         self.setMovable(False)
 
         self.__foldBtn = SvgIconPushButton()
@@ -27,13 +30,25 @@ class FoldableToolBar(QToolBar):
 
         self.addAction(self.__foldAction)
 
+        self.__menuAnimation = QPropertyAnimation(self, b"height")
+        self.__menuAnimation.valueChanged.connect(self.setFixedHeight)
+
+        self.__menuAnimation.setStartValue(self.sizeHint().height())
+        self.__menuAnimation.setDuration(200)  # default duration
+        self.__menuAnimation.setEndValue(self.__foldBtn.sizeHint().height())  # default end value
+
     def __fold(self, f):
         if f:
+            self.__menuAnimation.setDirection(QAbstractAnimation.Forward)
+            self.__menuAnimation.start()
             self.__foldBtn.setIcon('ico/unfold.svg')
             self.setFixedHeight(self.__foldBtn.sizeHint().height())
         else:
+            self.__menuAnimation.setDirection(QAbstractAnimation.Backward)
+            self.__menuAnimation.start()
             self.__foldBtn.setIcon('ico/fold.svg')
             self.setFixedHeight(self.sizeHint().height())
 
     def addWidget(self, widget: QWidget) -> QAction:
         self.insertWidget(self.__foldAction, widget)
+        self.__menuAnimation.setStartValue(self.sizeHint().height())
